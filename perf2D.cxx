385c20b34fac017e61c240b7577aa84d3a3bef9b
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-08-04 13:11:54 +0200

    modify perf2D to support new algorithm for dilate only currently
    darcs-hash:20060804111154-2fc9d-34170c63f8a25214bc8dbc4ed70b3f8ec2594e89.gz

--- perf2D.cxx
+++ perf2D.cxx
@@ -1,18 +1,12 @@
 #include "itkImageFileReader.h"
 
-#include "itkMovingHistogramDilateImageFilter.h"
-#include "itkBasicDilateImageFilter.h"
 #include "itkGrayscaleDilateImageFilter.h"
-
-#include "itkMovingHistogramErodeImageFilter.h"
-#include "itkBasicErodeImageFilter.h"
 #include "itkGrayscaleErodeImageFilter.h"
-
-#include "itkMovingHistogramMorphologicalGradientImageFilter.h"
-#include "itkBasicMorphologicalGradientImageFilter.h"
 #include "itkMorphologicalGradientImageFilter.h"
+#include "itkGrayscaleMorphologicalOpeningImageFilter.h"
+#include "itkGrayscaleMorphologicalClosingImageFilter.h"
 
-#include "itkBinaryBallStructuringElement.h"
+#include "itkFlatStructuringElement.h"
 #include "itkTimeProbe.h"
 #include <vector>
 #include "itkMultiThreader.h"
@@ -31,50 +25,29 @@ int main(int, char * argv[])
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
   
-  typedef itk::BinaryBallStructuringElement< PType, dim > SRType;
-  
-  typedef itk::MovingHistogramDilateImageFilter< IType, IType, SRType > HDilateType;
-  HDilateType::Pointer hdilate = HDilateType::New();
-  hdilate->SetInput( reader->GetOutput() );
-  
-  typedef itk::BasicDilateImageFilter< IType, IType, SRType > BDilateType;
-  BDilateType::Pointer bdilate = BDilateType::New();
+  typedef itk::FlatStructuringElement< dim > SRType;
+  typedef itk::GrayscaleDilateImageFilter< IType, IType, SRType > DilateType;
+  typedef itk::GrayscaleErodeImageFilter< IType, IType, SRType > ErodeType;
+  typedef itk::MorphologicalGradientImageFilter< IType, IType, SRType > GradientType;
+  typedef itk::GrayscaleMorphologicalOpeningImageFilter< IType, IType, SRType > OpenType;
+  typedef itk::GrayscaleMorphologicalClosingImageFilter< IType, IType, SRType > CloseType;
+
+  DilateType::Pointer bdilate = DilateType::New();
   bdilate->SetInput( reader->GetOutput() );
   
-  typedef itk::GrayscaleDilateImageFilter< IType, IType, SRType > DilateType;
-  DilateType::Pointer dilate = DilateType::New();
-  dilate->SetInput( reader->GetOutput() );
-
+  DilateType::Pointer hdilate = DilateType::New();
+  hdilate->SetInput( reader->GetOutput() );
   
-  typedef itk::MovingHistogramErodeImageFilter< IType, IType, SRType > HErodeType;
-  HErodeType::Pointer herode = HErodeType::New();
-  herode->SetInput( reader->GetOutput() );
+  DilateType::Pointer adilate = DilateType::New();
+  adilate->SetInput( reader->GetOutput() );
   
-  typedef itk::BasicErodeImageFilter< IType, IType, SRType > BErodeType;
-  BErodeType::Pointer berode = BErodeType::New();
-  berode->SetInput( reader->GetOutput() );
-  
-  typedef itk::GrayscaleErodeImageFilter< IType, IType, SRType > ErodeType;
-  ErodeType::Pointer erode = ErodeType::New();
-  erode->SetInput( reader->GetOutput() );
-
-  
-  typedef itk::MovingHistogramMorphologicalGradientImageFilter< IType, IType, SRType > HMorphologicalGradientType;
-  HMorphologicalGradientType::Pointer hgradient = HMorphologicalGradientType::New();
-  hgradient->SetInput( reader->GetOutput() );
-  
-  typedef itk::MorphologicalGradientImageFilter< IType, IType, SRType > MorphologicalGradientType;
-  MorphologicalGradientType::Pointer gradient = MorphologicalGradientType::New();
-  gradient->SetInput( reader->GetOutput() );
-  
-  typedef itk::BasicMorphologicalGradientImageFilter< IType, IType, SRType > BMorphologicalGradientType;
-  BMorphologicalGradientType::Pointer bgradient = BMorphologicalGradientType::New();
-  bgradient->SetInput( reader->GetOutput() );
+  DilateType::Pointer vdilate = DilateType::New();
+  vdilate->SetInput( reader->GetOutput() );
   
   reader->Update();
   
   std::vector< int > radiusList;
-  for( int s=0; s<=10; s++)
+  for( int s=1; s<=10; s++)
     { radiusList.push_back( s ); }
   for( int s=15; s<=30; s+=5)
     { radiusList.push_back( s ); }
@@ -83,59 +56,32 @@ int main(int, char * argv[])
   radiusList.push_back( 100 );
   
   std::cout << "#radius" << "\t" 
-            << "rep" << "\t" 
-            << "total" << "\t" 
-            << "nb" << "\t" 
-            << "hnb" << "\t" 
             << "bd" << "\t" 
             << "hd" << "\t" 
-            << "d" << "\t" 
-            << "be" << "\t" 
-            << "he" << "\t" 
-            << "e" << "\t" 
-            << "bg" << "\t" 
-            << "hg" << "\t" 
-            << "g" << "\t" 
+            << "ad" << "\t" 
+            << "vd" << "\t" 
             << std::endl;
 
   for( std::vector< int >::iterator it=radiusList.begin(); it !=radiusList.end() ; it++)
     {
-    itk::TimeProbe dtime;
     itk::TimeProbe bdtime;
     itk::TimeProbe hdtime;
-
-    itk::TimeProbe etime;
-    itk::TimeProbe hetime;
-    itk::TimeProbe betime;
-  
-    itk::TimeProbe gtime;
-    itk::TimeProbe bgtime;
-    itk::TimeProbe hgtime;
-  
-    SRType kernel;
-    kernel.SetRadius( *it );
-    kernel.CreateStructuringElement();
-
-    // compute the number of activated neighbors in the structuring element
-    unsigned long nbOfNeighbors = 0;
-    for( SRType::Iterator nit=kernel.Begin(); nit!=kernel.End(); nit++ )
-      {
-      if( *nit > 0 )
-        { nbOfNeighbors++; }
-      }
+    itk::TimeProbe adtime;
+    itk::TimeProbe vdtime;
 
   
-    dilate->SetKernel( kernel );
+    SRType::RadiusType rad;
+    rad.Fill( *it );
+    SRType kernel = SRType::Box( rad );
+
     bdilate->SetKernel( kernel );
     hdilate->SetKernel( kernel );
-    
-    erode->SetKernel( kernel );
-    berode->SetKernel( kernel );
-    herode->SetKernel( kernel );
-    
-    gradient->SetKernel( kernel );
-    bgradient->SetKernel( kernel );
-    hgradient->SetKernel( kernel );
+    adilate->SetKernel( kernel );
+    vdilate->SetKernel( kernel );
+    bdilate->SetAlgorithm( DilateType::BASIC );
+    hdilate->SetAlgorithm( DilateType::HISTO );
+    adilate->SetAlgorithm( DilateType::ANCHOR );
+    vdilate->SetAlgorithm( DilateType::VHGW );
 
     int nbOfRepeats;
     if( *it <= 10 )
@@ -150,10 +96,6 @@ int main(int, char * argv[])
 
     for( int i=0; i<nbOfRepeats; i++ )
       {
-      dtime.Start();
-      dilate->Update();
-      dtime.Stop();
-      dilate->Modified();
       bdtime.Start();
       bdilate->Update();
       bdtime.Stop();
@@ -163,48 +105,24 @@ int main(int, char * argv[])
       hdilate->Update();
       hdtime.Stop();
       hdilate->Modified();
-      
-      etime.Start();
-      erode->Update();
-      etime.Stop();
-      erode->Modified();
-      hetime.Start();
-      herode->Update();
-      hetime.Stop();
-      herode->Modified();
-      betime.Start();
-      berode->Update();
-      betime.Stop();
-      berode->Modified();
-      
-      gtime.Start();
-      gradient->Update();
-      gtime.Stop();
-      gradient->Modified();
-      bgtime.Start();
-      bgradient->Update();
-      bgtime.Stop();
-      bgradient->Modified();
-      hgtime.Start();
-      hgradient->Update();
-      hgtime.Stop();
-      hgradient->Modified();
+
+      adtime.Start();
+      adilate->Update();
+      adtime.Stop();
+      adilate->Modified();
+
+      vdtime.Start();
+      vdilate->Update();
+      vdtime.Stop();
+      vdilate->Modified();
+
       }
       
     std::cout << std::setprecision(3) << *it << "\t" 
-              << nbOfRepeats << "\t" 
-              << (*it*2+1)*(*it*2+1) << "\t" 
-              << nbOfNeighbors << "\t"
-              << hdilate->GetPixelsPerTranslation() << "\t" 
               << bdtime.GetMeanTime() << "\t" 
               << hdtime.GetMeanTime() << "\t" 
-              << dtime.GetMeanTime() << "\t" 
-              << betime.GetMeanTime() << "\t" 
-              << hetime.GetMeanTime()<< "\t" 
-              << etime.GetMeanTime()<< "\t" 
-              << bgtime.GetMeanTime() << "\t" 
-              << hgtime.GetMeanTime() << "\t"
-              << gtime.GetMeanTime() << "\t" 
+              << adtime.GetMeanTime() << "\t" 
+              << vdtime.GetMeanTime() << "\t" 
 //<< erode->GetNameOfBackendFilterClass()
               <<std::endl;
     }
