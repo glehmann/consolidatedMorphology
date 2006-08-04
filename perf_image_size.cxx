3915db9dfd16ac2baf522cc263c1991e2feb0ff7
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-08-04 14:47:16 +0200

    update perf_image_size
    darcs-hash:20060804124716-2fc9d-300b080271f9ad9c252a16eeb229317d5f181998.gz

--- perf_image_size.cxx
+++ perf_image_size.cxx
@@ -1,12 +1,12 @@
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "itkMovingHistogramDilateImageFilter.h"
-#include "itkBasicDilateImageFilter.h"
+#include "itkGrayscaleDilateImageFilter.h"
 #include "itkCropImageFilter.h"
-#include "itkNeighborhood.h"
+#include "itkFlatStructuringElement.h"
 #include "itkTimeProbe.h"
 #include <vector>
 #include "itkMultiThreader.h"
+#include <iomanip>
 
 int main(int, char * argv[])
 {
@@ -25,41 +25,54 @@ int main(int, char * argv[])
   CropType::Pointer crop = CropType::New();
   crop->SetInput( reader->GetOutput() );
   
-  typedef itk::Neighborhood<PType, dim> SRType;
-  SRType kernel;
-  kernel.SetRadius( 5 );
-  unsigned long nbOfNeighbors = 0;
-  for( SRType::Iterator kit=kernel.Begin(); kit!=kernel.End(); kit++ )
-    { *kit = 1; }
+  typedef itk::FlatStructuringElement<dim> SRType;
+  SRType::RadiusType rad;
+  rad.Fill( 3 );
+  SRType kernel = SRType::Box( rad );
+
+  typedef itk::GrayscaleDilateImageFilter< IType, IType, SRType > DilateType;
+  DilateType::Pointer bdilate = DilateType::New();
+  bdilate->SetInput( crop->GetOutput() );
+  bdilate->SetKernel( kernel );
+  bdilate->SetAlgorithm( DilateType::BASIC );
   
-  typedef itk::MovingHistogramDilateImageFilter< IType, IType, SRType > HDilateType;
-  HDilateType::Pointer hdilate = HDilateType::New();
+  DilateType::Pointer hdilate = DilateType::New();
   hdilate->SetInput( crop->GetOutput() );
   hdilate->SetKernel( kernel );
+  hdilate->SetAlgorithm( DilateType::HISTO );
   
-  typedef itk::BasicDilateImageFilter< IType, IType, SRType > DilateType;
-  DilateType::Pointer dilate = DilateType::New();
-  dilate->SetInput( crop->GetOutput() );
-  dilate->SetKernel( kernel );
+  DilateType::Pointer adilate = DilateType::New();
+  adilate->SetInput( crop->GetOutput() );
+  adilate->SetKernel( kernel );
+  adilate->SetAlgorithm( DilateType::ANCHOR );
+  
+  DilateType::Pointer vdilate = DilateType::New();
+  vdilate->SetInput( crop->GetOutput() );
+  vdilate->SetKernel( kernel );
+  vdilate->SetAlgorithm( DilateType::VHGW );
   
   reader->Update();
   
-  std::cout << "#radius" << "\t" 
-            << "rep" << "\t" 
-            << "total" << "\t" 
-            << "nb" << "\t" 
-            << "hnb" << "\t" 
-            << "im size" << "\t" 
-            << "d" << "\t" 
-            << "hd" << std::endl;
+  std::cout << "#size" << "\t" 
+            << "bd" << "\t" 
+            << "hd" << "\t" 
+            << "ad" << "\t" 
+            << "vd" << "\t" 
+            << "bd/p" << "\t" 
+            << "hd/p" << "\t" 
+            << "ad/p" << "\t" 
+            << "vd/p" << "\t" 
+            << std::endl;
 
   IType::SizeType imSize = reader->GetOutput()->GetLargestPossibleRegion().GetSize();
   IType::SizeType size = imSize;
 
   for( int i=0; i<=25; i++)
     {
-    itk::TimeProbe dtime;
+    itk::TimeProbe bdtime;
     itk::TimeProbe hdtime;
+    itk::TimeProbe adtime;
+    itk::TimeProbe vdtime;
 
     IType::SizeType cropSize = imSize - size;
     for(int j=0; j<dim; j++)
@@ -71,29 +84,43 @@ int main(int, char * argv[])
 
     for( int i=0; i<nbOfRepeats; i++ )
       {
-      dtime.Start();
-      dilate->UpdateLargestPossibleRegion();
-      dtime.Stop();
-      dilate->Modified();
+      bdtime.Start();
+      bdilate->UpdateLargestPossibleRegion();
+      bdtime.Stop();
+      bdilate->Modified();
 
       hdtime.Start();
       hdilate->UpdateLargestPossibleRegion();
       hdtime.Stop();
       hdilate->Modified();
+
+      adtime.Start();
+      adilate->UpdateLargestPossibleRegion();
+      adtime.Stop();
+      adilate->Modified();
+
+      vdtime.Start();
+      vdilate->UpdateLargestPossibleRegion();
+      vdtime.Stop();
+      vdilate->Modified();
+
       }
       
     unsigned long imageSize = 1;
     for( int j=0; j<dim; j++)
       { imageSize *= crop->GetOutput()->GetLargestPossibleRegion().GetSize()[j]; }
 
-    std::cout << 5 << "\t" 
-              << nbOfRepeats << "\t" 
-              << 121 << "\t" 
-              << 121 << "\t"
-              << hdilate->GetPixelsPerTranslation() << "\t" 
+    std::cout << std::setprecision(3)
               << imageSize << "\t" 
-              << dtime.GetMeanTime() << "\t" 
-              << hdtime.GetMeanTime() << std::endl;
+              << bdtime.GetMeanTime() << "\t" 
+              << hdtime.GetMeanTime() << "\t" 
+              << adtime.GetMeanTime() << "\t" 
+              << vdtime.GetMeanTime() << "\t" 
+              << bdtime.GetMeanTime() / (double) imageSize * 1000000000 << "\t" 
+              << hdtime.GetMeanTime() / (double) imageSize * 1000000000 << "\t" 
+              << adtime.GetMeanTime() / (double) imageSize * 1000000000 << "\t" 
+              << vdtime.GetMeanTime() / (double) imageSize * 1000000000 << "\t" 
+              << std::endl;
 
     for(int j=0; j<dim; j++)
       { size[j] = static_cast<unsigned long>(size[j] * 0.85f); }
