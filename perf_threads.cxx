6248569efb865e7585542438fa603b9e9a608156
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-09-13 10:29:38 +0200

    thread perfs also for van Herk and Anchor algorithms
    darcs-hash:20060913082938-2fc9d-eecbf93b13b3dd97984b2da231e45a119443a737.gz

--- perf_threads.cxx
+++ perf_threads.cxx
@@ -2,9 +2,12 @@
 #include "itkImageFileWriter.h"
 #include "itkMovingHistogramDilateImageFilter.h"
 #include "itkBasicDilateImageFilter.h"
-#include "itkBinaryBallStructuringElement.h"
+#include "itkvHGWDilateImageFilter.h"
+#include "itkAnchorDilateImageFilter.h"
 #include "itkTimeProbe.h"
 #include <vector>
+#include "itkFlatStructuringElement.h"
+#include <iomanip>
 
 int main(int, char * argv[])
 {
@@ -17,10 +20,10 @@ int main(int, char * argv[])
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
   
-  typedef itk::BinaryBallStructuringElement< PType, dim > SRType;
-  SRType kernel;
-  kernel.SetRadius( 4 );
-  kernel.CreateStructuringElement();
+  typedef itk::FlatStructuringElement< dim > SRType;
+  SRType::RadiusType rad;
+  rad.Fill( 4 );
+  SRType kernel = SRType::Box( rad );
   
   typedef itk::MovingHistogramDilateImageFilter< IType, IType, SRType > HDilateType;
   HDilateType::Pointer hdilate = HDilateType::New();
@@ -32,6 +35,16 @@ int main(int, char * argv[])
   dilate->SetInput( reader->GetOutput() );
   dilate->SetKernel( kernel );
   
+  typedef itk::vHGWDilateImageFilter< IType, SRType > VHDilateType;
+  VHDilateType::Pointer vhdilate = VHDilateType::New();
+  vhdilate->SetInput( reader->GetOutput() );
+  vhdilate->SetKernel( kernel );
+  
+  typedef itk::AnchorDilateImageFilter< IType, SRType > ADilateType;
+  ADilateType::Pointer adilate = ADilateType::New();
+  adilate->SetInput( reader->GetOutput() );
+  adilate->SetKernel( kernel );
+  
 /*  // write 
   typedef itk::ImageFileWriter< IType > WriterType;
   WriterType::Pointer writer = WriterType::New();
@@ -42,15 +55,21 @@ int main(int, char * argv[])
   
   std::cout << "#nb" << "\t" 
             << "d" << "\t" 
-            << "hd" << std::endl;
+            << "hd" << "\t"
+            << "ad" << "\t"
+            << "vhd" << std::endl;
 
-  for( int t=1; t<=4; t++ )
+  for( int t=1; t<=10; t++ )
     {
     itk::TimeProbe time;
     itk::TimeProbe htime;
+    itk::TimeProbe vhtime;
+    itk::TimeProbe atime;
   
+    dilate->SetNumberOfThreads( t );
     hdilate->SetNumberOfThreads( t );
-    hdilate->SetNumberOfThreads( t );
+    vhdilate->SetNumberOfThreads( t );
+    adilate->SetNumberOfThreads( t );
     
     for( int i=0; i<50; i++ )
       {
@@ -62,13 +81,25 @@ int main(int, char * argv[])
       hdilate->Update();
       htime.Stop();
       
+      vhtime.Start();
+      vhdilate->Update();
+      vhtime.Stop();
+      
+      atime.Start();
+      adilate->Update();
+      atime.Stop();
+      
       dilate->Modified();
       hdilate->Modified();
+      vhdilate->Modified();
+      adilate->Modified();
       }
       
-    std::cout << t << "\t" 
+    std::cout << std::setprecision(3) << t << "\t" 
               << time.GetMeanTime() << "\t"
-              << htime.GetMeanTime() << std::endl;
+              << htime.GetMeanTime() << "\t"
+              << atime.GetMeanTime() << "\t"
+              << vhtime.GetMeanTime() << std::endl;
     }
   
   
