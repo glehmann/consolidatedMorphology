8381b2b4d5d3893b43b1f279e4103d15430a2078
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-11-06 16:28:06 +0100

    add IterativeImageFilter
    darcs-hash:20061106152806-2fc9d-b53cf886e42b5c3d481dc7d79e18954fee2f766c.gz

---
+++ itkIterativeImageFilter.h
@@ -0,0 +1,87 @@
+#ifndef __itkIterativeImageFilter_h
+#define __itkIterativeImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkCastImageFilter.h"
+
+
+namespace itk {
+
+/**
+ * \class IterativeImageFilter
+ * \brief A iterative filter to apply N times a transform to an image
+ *
+ * \author Gaetan Lehmann
+ */
+
+template<class TInputImage, class TOutputImage>
+class ITK_EXPORT IterativeImageFilter : 
+public ImageToImageFilter<TInputImage, TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef IterativeImageFilter Self;
+  typedef ImageToImageFilter<TInputImage,TOutputImage>  Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+  
+  /** Standard New method. */
+  itkNewMacro(Self);
+
+  /** Runtime information support. */
+  itkTypeMacro(IterativeImageFilter,
+               ImageToImageFilter);
+ 
+  /** Image related typedefs. */
+  typedef TInputImage InputImageType;
+  typedef typename TInputImage::RegionType RegionType ;
+  typedef typename TInputImage::SizeType SizeType ;
+  typedef typename TInputImage::IndexType IndexType ;
+  typedef typename TInputImage::PixelType PixelType ;
+  typedef typename TInputImage::OffsetType OffsetType ;
+  
+  typedef TOutputImage OutputImageType;
+  typedef typename TOutputImage::PixelType OutputPixelType ;
+
+  typedef ImageToImageFilter<TOutputImage, TOutputImage> FilterType ;
+  typedef CastImageFilter< InputImageType, OutputImageType > CastType ;
+  
+  /** Image related typedefs. */
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+  /** n-dimensional Kernel radius. */
+  typedef typename TInputImage::SizeType RadiusType ;
+
+  itkSetMacro(NumberOfIterations, unsigned int);
+  itkGetMacro(NumberOfIterations, unsigned int);
+
+  itkSetObjectMacro(Filter, FilterType);
+  itkGetObjectMacro(Filter, FilterType);
+
+  void GenerateInputRequestedRegion() ;
+  void EnlargeOutputRequestedRegion(DataObject *itkNotUsed(output));
+
+protected:
+  IterativeImageFilter();
+  ~IterativeImageFilter() {};
+
+  void GenerateData();
+
+  typename FilterType::Pointer m_Filter;
+  
+  typename CastType::Pointer m_Cast;
+
+  unsigned int m_NumberOfIterations;
+
+private:
+  IterativeImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+};
+
+}
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkIterativeImageFilter.txx"
+#endif
+
+#endif
---
+++ itkIterativeImageFilter.txx
@@ -0,0 +1,81 @@
+#ifndef __itkIterativeImageFilter_txx
+#define __itkIterativeImageFilter_txx
+
+#include "itkIterativeImageFilter.h"
+#include "itkProgressAccumulator.h"
+
+namespace itk {
+
+template <class TInputImage, class TOutputImage>
+IterativeImageFilter<TInputImage, TOutputImage>
+::IterativeImageFilter()
+{
+  m_Cast = CastType::New();
+  m_Cast->SetInPlace( true );
+  m_Cast->ReleaseDataFlagOn();
+  m_Filter = NULL;
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+IterativeImageFilter<TInputImage, TOutputImage>
+::GenerateInputRequestedRegion()
+{
+  // call the superclass' implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+
+  typename InputImageType::Pointer  inputPtr =
+    const_cast< InputImageType * >( this->GetInput() );
+
+  if ( !inputPtr )
+    { return; }
+
+  inputPtr->SetRequestedRegion(inputPtr->GetLargestPossibleRegion());
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+IterativeImageFilter<TInputImage, TOutputImage>
+::EnlargeOutputRequestedRegion(DataObject *)
+{
+  this->GetOutput()->SetRequestedRegion( this->GetOutput()->GetLargestPossibleRegion() );
+}
+
+
+template <class TInputImage, class TOutputImage>
+void
+IterativeImageFilter<TInputImage, TOutputImage>
+::GenerateData()
+{
+  if( !m_Filter )
+    {
+    itkExceptionMacro("Filter must be set.");
+    }
+
+  this->AllocateOutputs();
+  // set up the pipeline
+  m_Cast->SetInput( this->GetInput() );
+
+  // Create a process accumulator for tracking the progress of this minipipeline
+  ProgressAccumulator::Pointer progress = ProgressAccumulator::New();
+  progress->SetMiniPipelineFilter(this);
+  typename OutputImageType::Pointer img = m_Cast->GetOutput();
+   for( int i=0; i<m_NumberOfIterations; i++ )
+     {
+     m_Filter->SetInput( img );
+     progress->RegisterInternalFilter(m_Filter, 1.0/m_NumberOfIterations);
+     m_Filter->Update();
+     img = m_Filter->GetOutput();
+     img->DisconnectPipeline();
+     }
+
+  this->GraftOutput( img );
+
+}
+
+}
+
+
+#endif
